ae258b73b655cc718b59d64ba676c70348af633a
Kevin Boyd <kevin.boyd@gmail.com>
2022-07-09 13:30:48 -0700

    Set the name and version

--- src/Game.php
+++ src/Game.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Application;
 
 class Game extends Application
 {
-    public function __construct(string $name = 'UNKNOWN', string $version = 'UNKNOWN')
+    public function __construct(string $name = 'Whateverthing: The One-Page RPG Engine', string $version = '1.0.0')
     {
         parent::__construct($name, $version);
 
